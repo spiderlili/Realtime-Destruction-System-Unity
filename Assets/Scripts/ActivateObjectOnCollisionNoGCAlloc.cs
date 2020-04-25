f27b22989094ebcca18654a04c55f0c75e0d5e87
Jing <jtan1993@gmail.com>
2020-04-26 00:30:57 +0100

    added noGCAlloc code

---
+++ Assets/Scripts/ActivateObjectOnCollisionNoGCAlloc.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObjectOnCollisionNoGCAlloc : MonoBehaviour
+{
+    public float activationForce = 20; //compare against each rigid body - trigger the object if forces exceed this 
+    public GameObject[] objectsToActivate;
+
+    bool go = true;
+    float velocity;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if(rb == null || objectsToActivate == null)
+        {
+            go = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (go)
+        {
+            if (!rb.isKinematic)
+            {
+                if (!rb.IsSleeping())
+                {
+                    var v = Mathf.Abs(rb.velocity.sqrMagnitude);
+                    if ((v - velocity) > activationForce)
+                    {
+                        foreach (GameObject objectToActivate in objectsToActivate)
+                        {
+                            objectToActivate.SetActive(true);
+                        }
+                        velocity = v;
+                    }
+                }
+            }
+        }
+    }
+}
